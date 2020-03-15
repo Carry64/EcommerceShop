b64be16321a56f552b35c7557f73b1f9d1dfd2ef
Anatoly Yovkov <42945703+Carry64@users.noreply.github.com>
2020-03-15 04:22:49 +0200

    Refactored some  of the code

--- EcommerceShop/EcommerceShop.DataAccess.InMemory/InMemoryRepository.cs
+++ EcommerceShop/EcommerceShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -1,14 +1,13 @@
-﻿using EcommerceShop.Core.Models;
+﻿using EcommerceShop.Core.Contracts;
+using EcommerceShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Caching;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EcommerceShop.DataAccess.InMemory
 {
-    public class InMemoryRepository<T> where T : BaseEntity
+    public class InMemoryRepository<T> : IRepository<T> where T : BaseEntity
     {
         ObjectCache cache = MemoryCache.Default;
         List<T> items;
--- EcommerceShop/EcommerceShop.WebUI/Controllers/ProductManagerController.cs
+++ EcommerceShop/EcommerceShop.WebUI/Controllers/ProductManagerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EcommerceShop.Core.Contracts;
 using EcommerceShop.Core.Models;
 using EcommerceShop.Core.ViewModels;
 using EcommerceShop.DataAccess.InMemory;
@@ -11,13 +12,13 @@ namespace EcommerceShop.WebUI.Controllers
 {
     public class ProductManagerController : Controller
     {
-        InMemoryRepository<Product> context;
-        InMemoryRepository<ProductCategory> productCategories;
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
 
-        public ProductManagerController()
+        public ProductManagerController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
         {
-            context = new InMemoryRepository<Product>();
-            productCategories = new InMemoryRepository<ProductCategory>();
+            context = productContext;
+            productCategories = productCategoryContext;
         }
 
         // GET: ProductManager
